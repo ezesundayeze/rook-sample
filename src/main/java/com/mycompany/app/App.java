4b1f5bb0ac52f4ff96ee0d6a52bd24bef9eb49a6
Eze Sunday Eze <mailsforeze@gmail.com>
2021-06-07 00:34:14 +0100

    add new class

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,17 +1,28 @@
 package com.mycompany.app;
+import io.javalin.Context;
+import io.javalin.Handler;
+import io.javalin.Javalin;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    do {
-        // do something
-                System.out.println( "Hello World!" );
-    } while (true);
+import com.rookout.rook.API;
+import com.rookout.rook.RookOptions;
 
+public class App {
+    public static void main(String[] args) {
+        Javalin app = Javalin.start(7000);
+
+        RookOptions opts = new RookOptions();
+        opts.token = "<Your-Token>";
+        API.start(opts);
+
+        app.get("/", new Handler() {
+            public void handle(Context ctx) throws Exception {
+                ctx.result("Hello World");
+            }
+        });
+        app.get("/hello/:name", new Handler() {
+            public void handle(Context ctx) throws Exception {
+                ctx.result("Hello: " + ctx.param("name"));
+            }
+        });
     }
 }
