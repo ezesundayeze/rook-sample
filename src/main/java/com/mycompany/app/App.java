72e706a0e96e092db4599f49a12452b555a7f193
Eze Sunday Eze <mailsforeze@gmail.com>
2021-06-07 09:16:47 +0100

    add javalin api

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,28 +1,15 @@
 package com.mycompany.app;
-import io.javalin.Context;
-import io.javalin.Handler;
 import io.javalin.Javalin;
 
-import com.rookout.rook.API;
-import com.rookout.rook.RookOptions;
+
 
 public class App {
     public static void main(String[] args) {
-        Javalin app = Javalin.start(7000);
-
-        RookOptions opts = new RookOptions();
-        opts.token = "<Your-Token>";
-        API.start(opts);
-
-        app.get("/", new Handler() {
-            public void handle(Context ctx) throws Exception {
-                ctx.result("Hello World");
-            }
-        });
-        app.get("/hello/:name", new Handler() {
-            public void handle(Context ctx) throws Exception {
-                ctx.result("Hello: " + ctx.param("name"));
-            }
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> {
+            // some code
+            ctx.result("Hello world");
+            ctx.status(201);
         });
     }
 }
